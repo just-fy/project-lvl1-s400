6a58639ac8c4154995ade8436458222ea043e970
Nastya Faizulina <just-fy@mail.ru>
2018-12-27 00:38:35 +0300

    4th step in project

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # brain-games-nastya0292
 
-[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
-[![Test Coverage](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage)](https://codeclimate.com/github/codeclimate/codeclimate/test_coverage)
+[![Maintainability](https://api.codeclimate.com/v1/badges/276d84fd7b2a88d0390a/maintainability)](https://codeclimate.com/github/just-fy/project-lvl1-s400/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/276d84fd7b2a88d0390a/test_coverage)](https://codeclimate.com/github/just-fy/project-lvl1-s400/test_coverage)
 [![Build Status](https://travis-ci.com/just-fy/project-lvl1-s400.svg?branch=master)](https://travis-ci.com/just-fy/project-lvl1-s400)
 
 ## Setup
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import readlineSync from 'readline-sync';
+
 const firstGreeting = 'Welcome to the Brain Games!';
+const taskOfEvenGame = 'Answer "yes" if number even otherwise answer "no".';
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
 
 const greeting = () => {
   console.log(firstGreeting);
+  console.log(taskOfEvenGame);
   const userName = readlineSync.question('What is your name? ', {
-    defaultInput: 'Friend'
+    defaultInput: 'Friend',
   });
   console.log(`Hello, ${userName}!`);
 };
 
+const isEven = value => value % 2 === 0;
+
+const brainEven = () => {
+  const randomNumber = getRandomInt(100);
+  console.log(`Question: ${randomNumber}`);
+  const answerStr = readlineSync.question('Your answer: ');
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  const ifCorrect = 'Correct!';
+  const ifWrong = `"${answerStr}" is wrong answer ;(. Correct answer was "${correctAnswer}".`;
+  return (answerStr === correctAnswer) ? console.log(ifCorrect) : console.log(ifWrong);
+};
+
 export { greeting };
+export { brainEven };
